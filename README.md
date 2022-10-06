22d61c680b2b426d1a6ef27672f7c622b2f8eaf1
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-06 23:10:40 +0200

    Create events app

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Django Event
-# Django Event
---
+++ events/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from accounts.models import User
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, blank=True)
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Submission(models.Model):
+    participant = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
+    details = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.event) + ' -- ' + str(self.participant)
