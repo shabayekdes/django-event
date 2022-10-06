36bacb9230528e3eda4c46f63b309b6705e61c8c
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-06 23:11:28 +0200

    Add details fields to user

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+    hackathon_participant = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
