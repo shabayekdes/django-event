3521c635c6bd1244b8e477342c772a6821c985fc
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-07 02:36:53 +0200

    Add event confirmation page

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:pk>/', views.show_event, name="show_event")
+    path('<str:pk>/', views.show_event, name="show_event"),
+    path('<str:pk>/confirmation/', views.event_confirmation, name="event_confirmation")
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from events.models import Event
 
@@ -6,6 +6,19 @@ from events.models import Event
 def show_event(request, pk):
     event = Event.objects.get(id=pk)
     context = {
-        'event': event
+        'event': event,
+        'participants': event.participants.all
     }
     return render(request, 'events/show_event.html', context)
+
+
+def event_confirmation(request, pk):
+    event = Event.objects.get(id=pk)
+    if request.method == "POST":
+        event.participants.add(request.user)
+        return redirect('show_event', pk=event.id)
+
+    context = {
+        'event': event
+    }
+    return render(request, 'events/event_confirmation.html', context)
