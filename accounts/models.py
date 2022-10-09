0ce6f1a741e1f3d5c0fd729a27a92e9cc439225a
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-10-09 22:45:46 +0200

    Implement uuid to migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,11 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     name = models.CharField(max_length=100, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
--- events/models.py
+++ events/models.py
@@ -1,10 +1,14 @@
+import uuid
+
 from django.db import models
 
 from accounts.models import User
 
 
 class Event(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     name = models.CharField(max_length=200)
+    banner = models.ImageField(upload_to='media/events', default='media/events/default.png')
     description = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(User, blank=True, related_name="events")
     date = models.DateTimeField()
@@ -16,6 +20,7 @@ class Event(models.Model):
 
 
 class Submission(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     participant = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
     details = models.TextField(null=True, blank=True)
